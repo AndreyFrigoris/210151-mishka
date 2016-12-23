e98236b616221b71ffd402d9bea89c977f5f1992
AndreyFrigoris <andreyfrigoris@gmail.com>
2016-12-23 19:27:27 +0200

    графика, попап

--- js/script.js
+++ js/script.js
@@ -21,3 +21,24 @@ navButton.addEventListener('click', function(event) {
     navToggle.classList.add('page-header__toggle--closed');
   }
 });
+
+var link = document.querySelectorAll('.modal-btn');
+
+if(link.length > 0) {
+  var modal = document.querySelector('.modal');
+  var overlay = document.querySelector('.overlay');
+
+  for(var i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      modal.classList.add('modal--show');
+      overlay.classList.add('overlay--show');
+    })
+  }
+
+  overlay.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.remove('modal--show');
+    overlay.classList.remove('overlay--show');
+  })
+}
